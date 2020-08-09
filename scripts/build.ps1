e7fba8d031d66c6e46172be202663cad06634361
Sean <seantsusmilch@gmail.com>
2020-08-08 20:24:16 -0500

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,7 +1,7 @@
 docker build -t slur-counter:1.0 .
 
 docker run -it --rm --name slur `
--v /home/sean/docker/slur/config:/config `
--v /home/sean/docker/slur/data:/data `
--v /home/sean/docker/slur/logs:/logs `
+-v /home/docker/slur/config:/config `
+-v /home/docker/slur/data:/data `
+-v /home/docker/slur/logs:/logs `
 slur-counter
