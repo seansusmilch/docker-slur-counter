68b07415855b45b089c12d8cc3b9bc7a1e4dda42
stummyhurt <seantsusmilch@gmail.com>
2021-01-16 11:50:23 -0600

    🛠 Added intents to support new version of discord.py

--- bin/slurs.py
+++ bin/slurs.py
@@ -11,18 +11,18 @@ class SlurCounter(commands.Bot):
     def __init__(self, token, logging, usermod, wordmod,
             reactions = True,
             silence = True) -> None:
-        super().__init__(command_prefix='!')
+        
+        intents = discord.Intents(messages=True, members=True, guilds=True)
+        super().__init__(command_prefix='!', intents=intents)
         self.log = logging
         self.usr = usermod
         self.wrd = wordmod
         # logging.info(f'Using token {token}')
-        print(f'registering cogs')
+        self.log.info(f'Registering cogs')
         self.add_cog(Scores(self, logging, usermod, wordmod))
-        print(f'Using token {token}')
         self.run(token)
 
     async def on_ready(self):
-        print(f'{self.user.name} has connected to Discord!')
         self.log.info(f'{self.user.name} has connected to Discord!')
 
     async def on_command_error(self, ctx, err):
